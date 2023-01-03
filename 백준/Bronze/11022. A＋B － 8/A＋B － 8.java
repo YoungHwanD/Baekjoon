9bc4d33c8914676986263c1bf55e07da99794fa6
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2023-01-04 03:28:15 +0900

    [Bronze V] Title: A+B - 8, Time: 304 ms, Memory: 18644 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int sum = 0;
+		
+		int[][] arr = new int[T][2];
+		
+		
+		for(int i = 0; i < T; i++) {
+			for(int j = 0; j < 2; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+        sc.close();        
+        
+		for(int i = 0; i < T; i++) {
+			for(int j = 0; j < 2; j++) {
+				sum += arr[i][j];
+			}
+			System.out.printf("Case #%d: %d + %d = %d\n", 
+					i+1, arr[i][0], arr[i][1], sum);
+			sum = 0;
+		}
+	}
+}
