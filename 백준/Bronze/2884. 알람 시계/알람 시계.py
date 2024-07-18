e3c4a301a9da23bd629dcb681faa8903d658473c
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-07-18 16:22:32 +0900

    [Bronze III] Title: 알람 시계, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,11 +1,11 @@
-hour, minute = input().split()
+hour, minute = map(int, input().split())
 
-hour = int(hour)
-minute = int(minute)
-
-if minute - 45 < 0 and hour - 1 < 0:
-  print(f"23 {(60 + (minute - 45))}")
-elif minute - 45 < 0:
-  print(f"{hour - 1} {(60 + (minute - 45))}")
-else:
-  print(f"{hour} {minute - 45}")
+if minute < 45:
+  if hour == 0:
+    hour = 23
+    minute += 60
+  else:
+    hour -= 1
+    minute += 60
+  
+print(hour, minute-45)
