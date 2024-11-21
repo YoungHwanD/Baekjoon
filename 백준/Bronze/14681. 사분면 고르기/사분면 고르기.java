8e1c280ac4ed16571ec5033bef2a80e2ea2f4c3f
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-11-22 01:13:11 +0900

    [Bronze V] Title: 사분면 고르기, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,25 +1,31 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n1 = sc.nextInt();
-		int n2 = sc.nextInt();
-		
-		if(n1 > 0) {
-			if(n2 > 0) {
-				System.out.println(1);
-			}else if(n2 < 0) {
-				System.out.println(4);
-			}
-		}else if(n1 < 0) {
-			if(n2 > 0) {
-				System.out.println(2);
-			}else if(n2 < 0) {
-				System.out.println(3);
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+//        int x = Integer.parseInt(st.nextToken());
+//        int y = Integer.parseInt(st.nextToken());
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
+        } else {
+            if (y > 0) {
+                System.out.println(2);
+            } else {
+                System.out.println(3);
+            }
+        }
+    }
 }
