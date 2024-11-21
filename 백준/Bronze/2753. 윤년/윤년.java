4323c99909644343df38948860494955c5787f25
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-11-21 14:27:22 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int year = sc.nextInt();
-		
-		if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
-			System.out.println(1);
-		}else {
-			System.out.println(0);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int y = Integer.parseInt(st.nextToken());
+
+        if ((y % 4 == 0) && ((y % 100 != 0) || (y % 400 == 0))) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
 }
