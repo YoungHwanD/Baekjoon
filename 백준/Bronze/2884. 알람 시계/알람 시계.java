3a606c3c38b54cd5b4e6269f4d06c06b292bb2af
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-11-22 01:31:53 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -9,18 +9,33 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int H = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken()) - 45;
+        
+//        int M = Integer.parseInt(st.nextToken());
 
-        if ((H == 0) && (M - 45 < 0)) {
-            H = 23;
-            M = M - 45 + 60;
-        } else if ((H > 0) && (M - 45 < 0)) {
-            H -= 1;
-            M = M - 45 + 60;
+//        if ((H == 0) && (M - 45 < 0)) {
+//            H = 23;
+//            M = M - 45 + 60;
+//        } else if ((H > 0) && (M - 45 < 0)) {
+//            H -= 1;
+//            M = M - 45 + 60;
+//        } else {
+//            M = M - 45;
+//        }
+//
+//        System.out.println(H + " " + M);
+
+        if (M < 0) {
+            if (H - 1 < 0) {
+                H = 23;
+                System.out.printf("%d %d", H, M + 60);
+            } else {
+                System.out.printf("%d %d", H - 1, M + 60);
+            }
         } else {
-            M = M - 45;
+            System.out.printf("%d %d", H, M);
         }
 
-        System.out.println(H + " " + M);
+
     }
 }
