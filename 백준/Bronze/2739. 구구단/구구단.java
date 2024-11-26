7c638f8aa11371c5b74f85b5a6a1fbaea78748ff
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-11-26 11:12:25 +0900

    [Bronze V] Title: 구구단, Time: 112 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,14 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		int n = sc.nextInt();
-
-		for (int i = 1; i <= 9; i++) {
-			System.out.printf("%d * %d = %d\n", n, i, n * i);
-		}
-	}
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d\n", N, i, N * i);
+        }
+    }
 }
