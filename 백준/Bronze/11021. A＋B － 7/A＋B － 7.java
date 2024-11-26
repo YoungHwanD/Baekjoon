dc93c515d8ffd6258feb9ab8aa56d7f4eb680eae
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-11-26 15:47:22 +0900

    [Bronze V] Title: A+B - 7, Time: 136 ms, Memory: 14948 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,27 +1,24 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int [] arr = new int[T];
 
-		int T = sc.nextInt();
-		int sum = 0;
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            arr[i] = A + B;
+        }
 
-		int[][] arr = new int[T][2];
+        for (int i = 0; i < T; i++) {
+            System.out.printf("Case #%d: %d\n", i + 1, arr[i]);
+        }
 
-		for (int i = 0; i < T; i++) {
-			for (int j = 0; j < 2; j++) {
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		for (int i = 0; i < T; i++) {
-			for (int j = 0; j < 2; j++) {
-				sum += arr[i][j];
-			}
-			System.out.printf("Case #%d: %d\n", i + 1, sum);
-			sum = 0;
-		}
-	}
-
+    }
 }
