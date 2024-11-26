2fc894093790b608477e94c2f33ec8a4e61ff6ab
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-11-26 16:44:40 +0900

    [Bronze V] Title: A+B - 5, Time: 116 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,41 +1,20 @@
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        for(;;) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
 
-//		int sum = 0;
-//		int[] arr = new int[2];
-//		
-//		
-//		for(int i = 0; i < 100; i++) {
-//			for(int j = 0; j < 2; j++) {
-//				arr[j] = sc.nextInt();
-//				if(arr[0] == 0 && arr[1] == 0) {
-//					break;
-//				}
-//			}
-//			
-//			for(int j = 0; j < 2; j++) {
-//				sum += arr[j];
-//			}
-//			System.out.println(sum);
-//			sum = 0;
-//			
-//		}
+            if (A == 0 && B == 0) {
+                break;
+            }
+            System.out.println(A + B);
+        }
 
-		for (;;) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-
-			if (A == 0 && B == 0) {
-				sc.close();
-				break;
-			}
-			System.out.println(A + B);
-		}
-
-	}
+    }
 }
