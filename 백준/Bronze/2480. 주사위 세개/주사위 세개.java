facc566799c03366af48eeaf54b0081138a004c0
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-11-26 11:06:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,28 +1,28 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[] arr = new int[3];
-		int max = 0;
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-		
-		for (int i = 0; i < 3; i++) {
-			arr[i] = sc.nextInt();
-		}
+        int m = 0;
 
-		if (arr[0] == arr[1] && arr[1] == arr[2]) {
-			System.out.println(10000 + (arr[0] * 1000));
-		}else if(arr[0] == arr[1] || arr[0] == arr[2]) {
-			System.out.println(1000 + (arr[0] * 100));
-		}else if(arr[1] == arr[2]) {
-			System.out.println(1000 + (arr[1] * 100));
-		}else {
-			System.out.print(Math.max(Math.max(arr[0], arr[1]), arr[2])* 100);
-		}
-
-	}
-
+        if (A == B && B == C) {
+            m = 10000 + (A * 1000);
+        } else if (A != B && A != C && B != C) {
+            m = Math.max(A, Math.max(B, C)) * 100;
+        } else if (A == B || B == C) {
+            m = 1000 + (B * 100);
+        } else {
+            m = 1000 + (A * 100);
+        }
+        System.out.println(m);
+    }
 }
