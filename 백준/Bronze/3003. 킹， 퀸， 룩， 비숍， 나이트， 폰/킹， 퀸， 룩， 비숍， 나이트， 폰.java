f41c1584b1bcb932a51f1e0754edcd7d8ea93e0a
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-12-03 15:50:24 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 112 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,19 +1,21 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[] arr = new int[6];
-		int[] set = new int[] {1, 1, 2, 2, 2, 8};
-		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(set[i] - arr[i] + " ");
-		}
-	}
+        int[] arr = {1, 1, 2, 2, 2, 8};
+        for (int i = 0; i < 6; i++) {
+            int N = Integer.parseInt(st.nextToken());
+            arr[i] -= N;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+    }
 }
