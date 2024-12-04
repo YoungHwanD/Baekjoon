13a6a2ea1092ab4f5300dc6bfb826543e168fdf0
YoungHwanD <107034988+YoungHwanD@users.noreply.github.com>
2024-12-04 09:53:39 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 276 ms, Memory: 22460 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < T; i++) {
+            int C = Integer.parseInt(br.readLine());
+
+            int Q = C / 25;
+            C %= 25;
+
+            int D = C / 10;
+            C %= 10;
+
+            int N = C / 5;
+            C %= 5;
+
+            int P = C;
+
+            System.out.printf("%d %d %d %d\n", Q, D, N, P);
+        }
+
+    }
+}
